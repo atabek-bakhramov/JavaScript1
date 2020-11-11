7bc173346e330f9e2b5e355f02637a9cf5cebc75
atabek-bakh <coderonline@icloud.com>
2020-11-11 17:31:35 +0100

    replaced if statement by ternary operator

--- Week1/homework/js-exercises/roundInteger-exercise5.js
+++ Week1/homework/js-exercises/roundInteger-exercise5.js
@@ -3,10 +3,5 @@ let z = 7.25;
 console.log(z);
 let a = Math.round(z);
 console.log(a);
-let highestValue;
-if (z > a) {
-    highestValue = z;
-} else {
-    highestValue = a;
-}
+let highestValue = (z > a) ? z : a;
 console.log(highestValue);
