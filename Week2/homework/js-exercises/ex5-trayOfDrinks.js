1012f5b9be0423d2e247a60d53201f697e072290
atabek-bakh <coderonline@icloud.com>
2020-11-15 14:52:09 +0100

    reordered some lines of code

--- Week2/homework/js-exercises/ex5-trayOfDrinks.js
+++ Week2/homework/js-exercises/ex5-trayOfDrinks.js
@@ -1,30 +1,30 @@
 'use strict'
 const drinkTypes = ['cola', 'lemonade', 'water'];
 let drinkTray = [];
-for (let i = 0; i < drinkTypes.length; i++) {
-    for (let j = 0; j < 2; j++) {
-        if (drinkTray.length < 5) {
-            drinkTray.push(drinkTypes[i]);
-        }
+while (drinkTray.length < 5) {
+    let randomDrink = Math.floor(Math.random() * drinkTypes.length);
+    if (drinkTray.indexOf(drinkTypes[randomDrink]) == -1 || drinkTray.indexOf(drinkTypes[randomDrink]) == drinkTray.lastIndexOf(drinkTypes[randomDrink])) {
+        drinkTray.push(drinkTypes[randomDrink]);
+    } else {
+        continue;
     }
 }
 console.log(`Hey guys, I brought a ${drinkTray.join(', ')}!`);
 
 
-// Some experimenting. To get everytime a different outcome (order of drinks), 
-// but still make sure not to get the same drink more than 2 times
+// Another solution
 //
 // const drinkTypes = ['cola', 'lemonade', 'water'];
 // let drinkTray = [];
-// while (drinkTray.length < 5) {
-//     let randomDrink = Math.floor(Math.random() * drinkTypes.length);
-//     if (drinkTray.indexOf(drinkTypes[randomDrink]) == -1 || drinkTray.indexOf(drinkTypes[randomDrink]) == drinkTray.lastIndexOf(drinkTypes[randomDrink])) {
-//         drinkTray.push(drinkTypes[randomDrink]);
-//     } else {
-//         continue;
+// for (let i = 0; i < drinkTypes.length; i++) {
+//     for (let j = 0; j < 2; j++) {
+//         if (drinkTray.length < 5) {
+//             drinkTray.push(drinkTypes[i]);
+//         }
 //     }
 // }
 // console.log(`Hey guys, I brought a ${drinkTray.join(', ')}!`);
+
 
 
 
--- Week2/homework/js-exercises/grade-calculator.js
+++ Week2/homework/js-exercises/grade-calculator.js
@@ -1,8 +1,8 @@
 'use strict'
 // the function expression accepts two parameters
-const gradeCalculator = (recievedScore, maxScore) => {
+const gradeCalculator = (receivedScore, maxScore) => {
     // converting scores into percentage
-    const percentage = recievedScore * 100 / maxScore;
+    const percentage = receivedScore * 100 / maxScore;
     switch (true) { // A question to the mentor: could you tell me please why it didn't work when I wrote (switch(percentage)). Wasn't it a truthy value? 
         case (percentage >= 90):
             console.log('Grade A (90% - 100%)');
