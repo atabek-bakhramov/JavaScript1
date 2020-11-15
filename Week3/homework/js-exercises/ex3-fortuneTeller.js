8a04198bfd487c629d268c40a278fc80a99c1255
atabek-bakh <coderonline@icloud.com>
2020-11-15 23:37:51 +0100

    finished exercise 3

--- Week3/homework/js-exercises/ex3-fortuneTeller.js
+++ Week3/homework/js-exercises/ex3-fortuneTeller.js
@@ -1,1 +1,18 @@
 'use strict'
+const numChildren = ['one', 'two', 'three', 'four', 'five'];
+const partnerNames = ['Obada', 'Clement', 'Maartje', 'Renske', 'Emmanuel'];
+const locations = ['Amsterdam', 'Utrecht', 'Eindhoven', 'Budel', 'Zwolle'];
+const jobs = ['software engineer', 'therapist', 'unemployed', 'Prime Minister', 'Batman'];
+
+const tellFortune = (numChildren, partnerNames, locations, jobs) => {
+    const randomNumberOfChildren = Math.floor(Math.random() * numChildren.length);
+    
+    const randomPartnerName = Math.floor(Math.random() * partnerNames.length);
+    const randomLocation = Math.floor(Math.random() * locations.length);
+    const randomJob = Math.floor(Math.random() * jobs.length);
+    console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kids.`)
+}
+
+tellFortune(numChildren, partnerNames, locations, jobs);
+tellFortune(numChildren, partnerNames, locations, jobs);
+tellFortune(numChildren, partnerNames, locations, jobs);
