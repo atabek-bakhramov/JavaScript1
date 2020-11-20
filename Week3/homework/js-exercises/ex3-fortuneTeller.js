dd3199ebdeb9dad51d6dd1e67d7d004d6844a9c9
atabek-bakh <coderonline@icloud.com>
2020-11-20 16:10:53 +0100

    added an if-else statement in the function

--- Week3/homework/js-exercises/ex3-fortuneTeller.js
+++ Week3/homework/js-exercises/ex3-fortuneTeller.js
@@ -1,16 +1,20 @@
 'use strict'
-const numChildren = ['one', 'two', 'three', 'four', 'five'];
-const partnerNames = ['Obada', 'Clement', 'Maartje', 'Renske', 'Emmanuel'];
-const locations = ['Amsterdam', 'Utrecht', 'Eindhoven', 'Budel', 'Zwolle'];
-const jobs = ['software engineer', 'therapist', 'unemployed', 'Prime Minister', 'Batman'];
 
-const tellFortune = (numChildren, partnerNames, locations, jobs) => {
+const numChildren = ['one', 'two', 'three', 'four', 'five'];
+const partnerNames = ['Obada', 'a cactus', 'Maartje', 'your computer', 'Emmanuel'];
+const locations = ['Amsterdam', 'Utrecht', 'Eindhoven', 'Budel', 'Zwolle'];
+const jobs = ['software engineer', 'therapist', 'rock-star', 'Prime Minister', 'Batman'];
+
+const tellFortune = (numChildren, partnerNames, locations, jobs) => { // a question to the mentor that checks my hw: is it okay to give names to parameters indentical to the arrays' names that already exist, like in this case?
     const randomNumberOfChildren = Math.floor(Math.random() * numChildren.length);
-    
     const randomPartnerName = Math.floor(Math.random() * partnerNames.length);
     const randomLocation = Math.floor(Math.random() * locations.length);
     const randomJob = Math.floor(Math.random() * jobs.length);
-    console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kids.`)
+    if (numChildren[randomNumberOfChildren] === 'one') {
+        console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kid.`)
+    } else {
+        console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kids.`)
+    }
 };
 
 tellFortune(numChildren, partnerNames, locations, jobs);
