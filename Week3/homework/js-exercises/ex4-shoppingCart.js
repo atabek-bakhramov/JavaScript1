506c0deaaff32a20e97f3f76fd795e59a35ee6c2
atabek-bakh <coderonline@icloud.com>
2020-11-20 13:01:35 +0100

    added a loop

--- Week3/homework/js-exercises/ex4-shoppingCart.js
+++ Week3/homework/js-exercises/ex4-shoppingCart.js
@@ -7,7 +7,21 @@ const addToShoppingCart = (groceryItem) => {
     if (shoppingCart.length > 3) {
         shoppingCart.shift();
     }
-    console.log(shoppingCart);
+    let fruitsYouBought = 'You bought';
+    shoppingCart.forEach((fruit) => {
+        fruitsYouBought += ` ${fruit}`;
+        if (shoppingCart.indexOf(fruit) === 2) {
+        fruitsYouBought += `!`;
+        }
+    })
+    // or another way
+    // for (let i = 0; i < shoppingCart.length; i++) {
+    //     fruitsYouBought += ` ${shoppingCart[i]}`;
+    //     if (shoppingCart.indexOf(shoppingCart[i]) === 2) {
+    //         fruitsYouBought += `!`;
+    //     }
+    // }
+    console.log(fruitsYouBought);
 };
 
 addToShoppingCart('chocolate'); // Returns "You bought bananas, milk, chocolate!"
