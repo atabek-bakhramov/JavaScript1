20007284a5373278b76ce6edfbcdcd24833c942a
atabek-bakh <coderonline@icloud.com>
2020-11-21 21:22:27 +0100

    a comment question added regarding a function created inside of another function

--- Week3/homework/js-exercises/ex3-fortuneTeller.js
+++ Week3/homework/js-exercises/ex3-fortuneTeller.js
@@ -5,7 +5,7 @@ const locations = ['Amsterdam', 'Utrecht', 'Eindhoven', 'Budel', 'Zwolle'];
 const jobs = ['software engineer', 'therapist', 'rock-star', 'Prime Minister', 'Batman'];
 
 const tellFortune = (numChildren, partnerNames, locations, jobs) => { // a question to the mentor that checks my hw: is it okay to give names to parameters indentical to the arrays' names that already exist, like in this case?
-    
+    // a question to the mentor: what would you suggest regarding the function 'getRandomIndex', should it be outside the 'tellFortune' function - in the global scope and just invoked inside of this function or this way is good as well? 
     function getRandomIndex(array) {
         const randomIndex = Math.floor(Math.random() * array.length);
         return randomIndex;
