75caa8073951b346a1687b5512b1224855d92c62
atabek-bakh <coderonline@icloud.com>
2020-11-21 12:01:19 +0100

    modified my fortuneTeller file

--- Week3/homework/js-exercises/ex3-fortuneTeller.js
+++ Week3/homework/js-exercises/ex3-fortuneTeller.js
@@ -1,22 +1,24 @@
 'use strict'
-
 const numChildren = ['one', 'two', 'three', 'four', 'five'];
 const partnerNames = ['Obada', 'a cactus', 'Maartje', 'your computer', 'Emmanuel'];
 const locations = ['Amsterdam', 'Utrecht', 'Eindhoven', 'Budel', 'Zwolle'];
 const jobs = ['software engineer', 'therapist', 'rock-star', 'Prime Minister', 'Batman'];
 
 const tellFortune = (numChildren, partnerNames, locations, jobs) => { // a question to the mentor that checks my hw: is it okay to give names to parameters indentical to the arrays' names that already exist, like in this case?
-    const randomNumberOfChildren = Math.floor(Math.random() * numChildren.length);
-    const randomPartnerName = Math.floor(Math.random() * partnerNames.length);
-    const randomLocation = Math.floor(Math.random() * locations.length);
-    const randomJob = Math.floor(Math.random() * jobs.length);
-    if (numChildren[randomNumberOfChildren] === 'one') {
-        console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kid.`)
-    } else {
-        console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kids.`)
+    
+    function getRandomIndex(array) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return randomIndex;
     }
-};
 
+    const randomNumberOfChildren = getRandomIndex(numChildren);
+    const randomPartnerName = getRandomIndex(partnerNames);
+    const randomLocation = getRandomIndex(locations);
+    const randomJob = getRandomIndex(jobs);
+
+    console.log(`You will be a ${jobs[randomJob]} in ${locations[randomLocation]}, married to ${partnerNames[randomPartnerName]} with ${numChildren[randomNumberOfChildren]} kid${numChildren[randomNumberOfChildren] === 'one' ? '' : 's'}.`) 
+    
+};
 tellFortune(numChildren, partnerNames, locations, jobs);
 tellFortune(numChildren, partnerNames, locations, jobs);
 tellFortune(numChildren, partnerNames, locations, jobs);
